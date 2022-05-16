cac85cf9398320dc78e52ae0734d66b2ad8ec48d
mdhasan-999 <webdeveloperhasan999@gmail.com>
2022-05-16 15:15:09 +0600

    Run build

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyDB1wqE4nCzqtD--orDZVpfRxkBsnpqU20",
+    authDomain: "docotr-portal-2e933.firebaseapp.com",
+    projectId: "docotr-portal-2e933",
+    storageBucket: "docotr-portal-2e933.appspot.com",
+    messagingSenderId: "424648637978",
+    appId: "1:424648637978:web:345f8fc3aa96f651961ba3"
 };
 
 // Initialize Firebase
